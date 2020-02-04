c92f0c8900e5c0f755495db5e800b336024b97ea
Michael Wixson <wixsonmc@gmail.com>
2020-02-04 03:35:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,18 +30,18 @@ def using_insert(array, element)
   array.insert(4, element)
 end
 
-def using_uniq
-  
+def using_uniq(array)
+  array.uniq
 end
 
 def using_flatten
-  
+  array.flatten
 end
 
-def using_delete
-  
+def using_delete(array,string)
+  array.delete(string)
 end
 
-def using_delete_at
-  
+def using_delete_at(array, integer)
+  array.delete[integer]
 end
