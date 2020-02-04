33f6102aa5493b0ca06db5d652bc885becb99e95
Michael Wixson <wixsonmc@gmail.com>
2020-02-04 03:25:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,3 +17,31 @@ end
 def using_shift
   
 end
+
+def shift_with_args
+  
+end
+
+def using_concat
+  
+end
+
+def using_insert
+
+end
+
+def using_uniq
+  
+end
+
+def using_flatten
+  
+end
+
+def using_delete
+  
+end
+
+def using_delete_at
+  
+end
